d8cc4cbfed84b3ad5494cb2e249e29ce28d66599
Alex <131786787+Spelks@users.noreply.github.com>
2024-02-17 19:42:15 +0000

    Add querySelectors for data attributes and css styles to eventListener.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const signUpBtn = document.querySelector(".sign-up-btn");
-const formSection = document.querySelector(".form-section");
+const signUpBtn = document.querySelector("[data-start-button]");
+const formSection = document.querySelector("[data-form]");
+const welcomeSection = document.querySelector("[data-welcome]");
+const welcomeContainer = document.querySelector(".welcome-container");
+const welcomeMessage = document.querySelector("[data-welcome-message]");
 
-signUpBtn.addEventListener("click", ()=> {
-    formSection.classList.remove("hide-form-section");
-    signUpBtn.style.display="none";
-})
+signUpBtn.addEventListener('click', ()=> {
+    signUpBtn.style.display = "none";
+    if (formSection.style.width === '0px' || formSection.style.width === '') {
+      formSection.style.width = '70%';
+      formSection.style.opacity = '1';
+      welcomeContainer.style.height = "358px";
+      welcomeSection.style.width = '30%';
+      welcomeMessage.style.opacity = "1";
+      welcomeMessage.textContent = "Join the Gestahlian Empire today, and you too can use magic derived from espers!";
+    }
+  });
+  
