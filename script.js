5595b7ea8fd60cf6a6175874bb2c52232910ca4c
Alex <131786787+Spelks@users.noreply.github.com>
2024-02-19 14:36:53 +0000

    Add credits color change and form rows to eventListener.

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const formSection = document.querySelector("[data-form]");
 const welcomeSection = document.querySelector("[data-welcome]");
 const welcomeContainer = document.querySelector(".welcome-container");
 const welcomeMessage = document.querySelector("[data-welcome-message]");
+const formRow = document.querySelectorAll(".form-row");
+const imageCredits = document.querySelector(".image-credits");
 
 signUpBtn.addEventListener('click', ()=> {
     signUpBtn.style.display = "none";
     if (formSection.style.width === '0px' || formSection.style.width === '') {
       formSection.style.width = '70%';
       formSection.style.opacity = '1';
-      welcomeContainer.style.height = "358px";
+      formRow.forEach((item) => item.style.opacity = "1");
+      // welcomeContainer.style.height = "358px";
+      welcomeContainer.style.height = "100%";
       welcomeSection.style.width = '30%';
       welcomeMessage.style.opacity = "1";
       welcomeMessage.textContent = "Join the Gestahlian Empire today, and you too can use magic derived from espers!";
+      imageCredits.style.color = "white";
+      imageCredits.style.borderTop = "1px solid white";
     }
   });
   
