cf9545a897dde0dcdd116ebc7159f5d7bc042282
Alex <131786787+Spelks@users.noreply.github.com>
2024-02-20 10:12:03 +0000

    Contain button code in function. Remove redundant 'if' statement.

--- script.js
+++ script.js
@@ -6,19 +6,18 @@ const welcomeMessage = document.querySelector("[data-welcome-message]");
 const formRow = document.querySelectorAll(".form-row");
 const imageCredits = document.querySelector(".image-credits");
 
-signUpBtn.addEventListener('click', ()=> {
-    signUpBtn.style.display = "none";
-    if (formSection.style.width === '0px' || formSection.style.width === '') {
-      formSection.style.width = '70%';
-      formSection.style.opacity = '1';
-      formRow.forEach((item) => item.style.opacity = "1");
-      // welcomeContainer.style.height = "358px";
-      welcomeContainer.style.height = "100%";
-      welcomeSection.style.width = '30%';
-      welcomeMessage.style.opacity = "1";
-      welcomeMessage.textContent = "Join the Gestahlian Empire today, and you too can use magic derived from espers!";
-      imageCredits.style.color = "white";
-      imageCredits.style.borderTop = "1px solid white";
-    }
-  });
+signUpBtn.addEventListener("click", signUpActive);
+
+function signUpActive() {
+  signUpBtn.style.display = "none";
+  formSection.style.cssText = "width: 70%; opacity: 1;";
+  formRow.forEach((item) => item.style.opacity = "1");
+  welcomeContainer.style.height = "100%";
+  welcomeSection.style.width = "30%";
+  welcomeMessage.style.opacity = "1";
+  welcomeMessage.textContent = "Join the Gestahlian Empire today, and you too can use magic derived from espers!";
+  // imageCredits.style.color = "white";
+  // imageCredits.style.borderTop = "1px solid white";
+  imageCredits.style.cssText = "color: white; border-top: 1px solid white;";  
+}
   
