ac9f5f477ebce6271d0c7559cac9ff8b6e963125
Alex <131786787+Spelks@users.noreply.github.com>
2024-02-28 19:54:36 +0000

    Add label eventListener. Add comments.

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ const welcomeMessage = document.querySelector("[data-welcome-message]");
 const formRow = document.querySelectorAll(".form-row");
 const imageCredits = document.querySelector(".image-credits");
 const gesFlag = document.querySelector(".ges-flag");
-
 const signUpForm = document.getElementById("sign-up-form");
 const accountBtn = document.getElementById("create-account");
-const passwordMtach = document.querySelector(".password-match");
+const passwordMatch = document.querySelector(".password-match");
+const inputs = document.querySelectorAll("input");
 
+// Event listeners for buttons.
 signUpBtn.addEventListener("click", signUpActive);
 accountBtn.addEventListener("click", matchPassword);
 
+// Activates the sign-up form and adjusts the layout when the 'Start Now' button is clicked.
 function signUpActive() {
   signUpBtn.style.display = "none";
   formSection.style.cssText = "width: 70%; opacity: 1;";
-  formRow.forEach((item) => item.style.opacity = "1");
+  formRow.forEach(item => item.style.opacity = "1");
   welcomeContainer.style.height = "100%";
   welcomeSection.style.width = "30%";
   welcomeMessage.style.cssText = "opacity: 1; margin-top: 20px;";
@@ -26,18 +28,25 @@ function signUpActive() {
   gesFlag.style.width = "160px";
 }
 
-function matchPassword() {
+// Validates that the password and confirm password fields match before form submission.
+function matchPassword(event) {
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("cpassword").value;
-  if(password === confirmPassword) {
-    passwordMtach.textContent = "";
-    return
+  if (password === confirmPassword) {
+    passwordMatch.textContent = "";
+  } else {
+    passwordMatch.textContent = "Password does not match!";
+    event.preventDefault(); // Prevent form submission if passwords do not match.
   }
-  passwordMtach.textContent = "Password does not match!";
-  event.preventDefault();
 }
 
-confirmPassword.classList.add("invalid");
-
-
-  
+// Applies class to input fields to prevent label overlap.
+inputs.forEach(input => {
+  input.addEventListener("blur", event => {
+    if (event.target.value) {
+      input.classList.add("is-valid");
+    } else {
+      input.classList.remove("is-valid");
+    }
+  });
+});
